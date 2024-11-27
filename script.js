3fe9c5bc874814277ecf69f4fdf0d32d5a5ea99a
ebbukhaeva <ebbukhaeva@gmail.com>
2024-11-27 15:56:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
-const coffees = [
+const sweaters = [
     {
       image: "001.JPG",
-      description: "Латте на кокосовом молоке",
-      price: 450
+      description: "Теплый свитер из шерсти",
+      price: 2500
     },
     {
       image: "002.JPG",
-      description: "Латте на банановом молоке",
-      price: 300
+      description: "Стильный свитер с узором",
+      price: 3000
     },
     {
       image: "003.JPG",
-      description: "Капучино с соленой карамелью",
-      price: 400
+      description: "Мягкий свитер из кашемира",
+      price: 4000
     },
     {
       image: "004.JPG",
-      description: "Классический американо",
-      price: 280
+      description: "Классический свитер с воротником",
+      price: 2800
     },
     {
-      image: "005.JPG ",
-      description: "Айс-латте",
-      price: 350
+      image: "005.JPG",
+      description: "Уютный свитер с капюшоном",
+      price: 3500
     },
     {
       image: "006.JPG",
-      description: "Смузи тропический",
-      price: 450
+      description: "Тонкий свитер из хлопка",
+      price: 2200
     },
     {
         image: "007.JPG",
-        description: "Смузи клубничный",
-        price: 400
+        description: "Тонкий свитер из хлопка",
+        price: 2200
       },
       {
         image: "008.JPG",
-        description: "Эспрессо",
-        price: 220
+        description: "Тонкий свитер из хлопка",
+        price: 2200
       },
       {
         image: "009.JPG",
-        description: "Раф лавандовый",
-        price: 350
+        description: "Тонкий свитер из хлопка",
+        price: 2200
       }
       
   ];
   
-  // Функция для создания блока с информацией о кофе
+  // Функция для создания блока с информацией о свитере
   function createProductBlock(sweater) {
     const block = document.createElement("div");
     block.classList.add("product-block");
